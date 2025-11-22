3d87f128ee2dfa46e719b7087c5b056990d7765c
Halex Viotto Gomes <halexx002@gmail.com>
2025-11-21 21:10:20 -0400

    fix: config to load env vars in host mode and container mode

--- configs/config.go
+++ configs/config.go
@@ -1,39 +1,67 @@
 package configs
 
 import (
+	"fmt"
+
 	"github.com/go-chi/jwtauth"
+	"github.com/go-playground/validator/v10"
 	"github.com/spf13/viper"
 )
 
 type conf struct {
-	IpMaxReqsBySec   int32  `mapstructure:"IP_MAX_REQS_BY_SEC"`
-	IpBlockTimeBySec int32  `mapstructure:"IP_BLOCK_TIME_BY_SEC"`
-	WebServerPort    string `mapstructure:"WEB_SERVER_PORT"`
-	RedisHost        string `mapstructure:"REDIS_HOST"`
-	RedisPort        string `mapstructure:"REDIS_PORT"`
-	JWTSecret        string `mapstructure:"JWT_SECRET"`
-	JWTExpiresIn     int    `mapstructure:"JWT_EXPIRES_IN"`
+	IpMaxReqsBySec   int32  `mapstructure:"IP_MAX_REQS_BY_SEC" validate:"required"`
+	IpBlockTimeBySec int32  `mapstructure:"IP_BLOCK_TIME_BY_SEC" validate:"required"`
+	WebServerPort    string `mapstructure:"WEB_SERVER_PORT" validate:"required"`
+	RedisHost        string `mapstructure:"REDIS_HOST" validate:"required"`
+	RedisPort        string `mapstructure:"REDIS_PORT" validate:"required"`
+	JWTSecret        string `mapstructure:"JWT_SECRET" validate:"required"`
+	JWTExpiresIn     int    `mapstructure:"JWT_EXPIRES_IN" validate:"required"`
 	TokenAuth        *jwtauth.JWTAuth
 }
 
 func LoadConfig(path string) (*conf, error) {
+	var cfg conf
 
-	var cfg *conf
-
-	viper.SetConfigName("app_config")
-	viper.SetConfigType("env")
 	viper.AddConfigPath(path)
-	viper.SetConfigFile(".env")
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
+	// ENV
 	viper.AutomaticEnv()
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(err)
+
+	// bind ENV VARS explicitamente
+	keys := []string{
+		"IP_MAX_REQS_BY_SEC",
+		"IP_BLOCK_TIME_BY_SEC",
+		"WEB_SERVER_PORT",
+		"REDIS_HOST",
+		"REDIS_PORT",
+		"JWT_SECRET",
+		"JWT_EXPIRES_IN",
 	}
-	err = viper.Unmarshal(&cfg)
-	if err != nil {
-		panic(err)
+	for _, key := range keys {
+		viper.BindEnv(key)
 	}
-	// Para assinar e gerar tokens JWT
+
+	// Try load .env
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			fmt.Println(".env não encontrado — usando apenas variáveis do ambiente")
+		} else {
+			return nil, err
+		}
+	}
+
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	if err := validate.Struct(&cfg); err != nil {
+		return nil, err
+	}
+
 	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
-	return cfg, err
+
+	return &cfg, nil
 }
--- go.mod
+++ go.mod
@@ -5,9 +5,17 @@ go 1.24.3
 require (
 	github.com/go-chi/chi/v5 v5.2.3
 	github.com/go-chi/jwtauth v1.2.0
+	github.com/go-playground/validator/v10 v10.28.0
 	github.com/redis/go-redis/v9 v9.16.0
 	github.com/spf13/viper v1.21.0
 	github.com/stretchr/testify v1.11.1
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 )
 
 require (
@@ -33,8 +41,8 @@ require (
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
